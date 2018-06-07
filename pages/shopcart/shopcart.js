99dd8db63b1ebf176a033816429f325da1c3942d
lisashare <lisa_zhu2012@foxmail.com>
2018-06-07 14:27:41 +0800

    计算总价，选择事件，全选事件

--- pages/shopcart/shopcart.js
+++ pages/shopcart/shopcart.js
@@ -1,91 +1,85 @@
 // pages/shopcart/shopcart.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    carts:[
-        {
-          pic: "http://mz.djmall.xmisp.cn/files/product/20161201/148058328876.jpg",
-          name:"日本资生堂洗颜",
-          price:200,
-          isSelect:false,
-          // 数据设定
-          count: {
-            quantity: 2,
-            min: 1,
-            max: 20
-          },
-        },
-        {
-          pic: 'http://mz.djmall.xmisp.cn/files/product/20161201/148058301941.jpg',
-          name: "倩碧焕妍活力精华露",
-          price: 340,
-          isSelect: false,
-          // 数据设定
-          count: {
-            quantity: 1,
-            min: 1,
-            max: 20
-          }
-        }
-    ]
+    carts:[],             //购物车列表
+    hasList:false,        //类表是否有数据
+    totalPrice:0,         //总价，初始为0
+    selectAllStatus:true  //全选状态，默认全选
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  onShow() {
+      this.setData({
+        hasList:true,
+        carts:[
+              {
+                id:1,
+                pic: "http://mz.djmall.xmisp.cn/files/product/20161201/148058328876.jpg",
+                name:"日本资生堂洗颜",
+                price:200,
+                isSelect:true,
+                count:1                
+              },
+              {
+                id:2,
+                pic: 'http://mz.djmall.xmisp.cn/files/product/20161201/148058301941.jpg',
+                name: "倩碧焕妍活力精华露",
+                price: 340,
+                isSelect: true,
+                count:4
+              }
+          ]
+      })
   },
+  //计算总价=选中的商品1的价格*数量 + 选中的商品2的 价格*数量 + ... 根据公式;页面中的其他操作会导致总价格变化的都需要调用该方法
+  getTotalPrice() {
+    let carts = this.data.carts; //获取购物车列表
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+    let total = 0;
+    //循环列表得到每个数据
+    for(let i = 0,len = carts.length;i < len;i++){
+      //判断选中才会计算价格
+      if(carts[i].isSelect){
+        //所有价格加起来
+        total += carts[i].count * carts[i].price;
+      }
+    }
+
+    this.setData({     //最后赋值到data中渲染到页面
+      carts:carts,  
+      totalPrice:total.toFixed(2)   
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  onReady() {
+    this.getTotalPrice()
   },
+  //选择事件：点击时选中，载点击又变成没选中状态，其实就是改变isSelect字段，通过data-index="{{index}}"把当前商品在类表数组中的下标传给事件
+  selectList(e) {
+    const index = e.currentTarget.dataset.index;//获取data-传递进来的index
+    let carts = this.data.carts;            //获取购物车列表
+    const isSelect = carts[index].isSelect; //获取当前商品的选中状态
+    carts[index].isSelect = !isSelect;      //改变状态
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+    this.setData({
+      carts:carts
+    })
+
+    this.getTotalPrice()  //重新获取总价
   },
+  //全选事件：全选就是根据全选状态selectAllStatus去改变每个商品的isSelect
+  selectAll() {
+    let selectAllStatus = this.data.selectAllStatus; //是否全选状态
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
+    selectAllStatus = !selectAllStatus;
+    let carts = this.data.carts;
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
+    for(let i = 0,len = carts.length;i < len;i++){
+      carts[i].isSelect = selectAllStatus;//改变所有商品的状态
+    }
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
+    this.setData({
+      selectAllStatus:selectAllStatus,
+      carts:carts
+    })
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    this.getTotalPrice()  //重新获取总价
   }
 })
